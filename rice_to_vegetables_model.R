be2a5ee4f1f59d1034b80b33b592baaed158b43c
Noviria Syifaun Nafsi <noviriasyifaunnafsi@gmail.com>
2023-12-29 11:27:46 +0100

    updated

--- rice_to_vegetables_model.R
+++ rice_to_vegetables_model.R
@@ -16,7 +16,7 @@ transition_rice_to_vegetables <- function(x, varnames){
   rice_water_use <- vv(rice_income/rice_water_use, n_year, var_CV=CV_value)
   rice_nutrition <- vv(rice_nutrition_to_health_value, n_year, var_CV=CV_value)
   
-  rice_total_benefit_precal <- sum(rice_income, rice_water_use, rice_nutrition)
+  rice_total_benefit_precal <- rice_income + rice_water_use + rice_nutrition
   rice_total_benefit <- vv(rice_total_benefit_precal, n_year, var_CV=CV_value)
   
   
@@ -27,7 +27,7 @@ transition_rice_to_vegetables <- function(x, varnames){
   potato_income <- vv(potato_yield*potato_price, n_year, var_CV=CV_value)
   shallot_income <- vv(shallot_yield*shallot_price, n_year, var_CV=CV_value)
   cabbage_income <- vv(cabbage_yield*cabbage_price, n_year, var_CV=CV_value) 
-  vegetables_income_precal <- sum(potato_income, shallot_income, cabbage_income)
+  vegetables_income_precal <- potato_income + shallot_income + cabbage_income
   vegetables_income <- vv(vegetables_income_precal, n_year, var_CV=CV_value)
   
   ###water use
@@ -37,7 +37,7 @@ transition_rice_to_vegetables <- function(x, varnames){
   vegetables_nutrition <- vv(vegetables_nutrition_to_health_value, n_year, var_CV=CV_value)
   
   ##vegetables total benefit
-  vegetables_total_benefit_precal <- sum(vegetables_income, vegetables_water_use, vegetables_nutrition)
+  vegetables_total_benefit_precal <- vegetables_income + vegetables_water_use + vegetables_nutrition
   vegetables_total_benefit <- vv(vegetables_total_benefit_precal, n_year, var_CV=CV_value)
   
   
@@ -52,20 +52,20 @@ transition_rice_to_vegetables <- function(x, varnames){
   
   
   ### total benefits crop rotation 1 
-  crop_rotation_income1_precal <- sum(potato_income, rice_income, shallot_income)
+  crop_rotation_income1_precal <- potato_income + rice_income + shallot_income
   crop_rotation_income1 <- vv(crop_rotation_income1_precal, n_year, var_CV=CV_value)
-  crop_rotation1_total_benefits_precal <- sum(crop_rotation_income1,
-                                      crop_rotation_water_use,
-                                      crop_rotation_nutrition)
+  crop_rotation1_total_benefits_precal <- crop_rotation_income1 +
+    crop_rotation_water_use +
+    crop_rotation_nutrition
   crop_rotation1_total_benefit <- vv(crop_rotation1_total_benefits_precal, n_year, var_CV=CV_value)
   
   
   ### total benefits crop rotation 2
   crop_rotation_income2_precal <- potato_income + rice_income + cabbage_income
   crop_rotation_income2 <- vv(crop_rotation_income2_precal, n_year, var_CV=CV_value)
-  crop_rotation2_total_benefits_precal <- sum(crop_rotation_income2,
-                                              crop_rotation_water_use,
-                                              crop_rotation_nutrition)
+  crop_rotation2_total_benefits_precal <- crop_rotation_income2 +
+    crop_rotation_water_use +
+    crop_rotation_nutrition
   crop_rotation2_total_benefit <- vv(crop_rotation2_total_benefits_precal, n_year, var_CV=CV_value)
   
   
@@ -73,65 +73,65 @@ transition_rice_to_vegetables <- function(x, varnames){
   ### total benefits crop rotation 3
   crop_rotation_income3_precal <- shallot_income + rice_income + cabbage_income
   crop_rotation_income3 <- vv(crop_rotation_income3_precal, n_year, var_CV=CV_value)
-  crop_rotation3_total_benefits_precal <- sum(crop_rotation_income3,
-                                              crop_rotation_water_use,
-                                              crop_rotation_nutrition)
+  crop_rotation3_total_benefits_precal <- crop_rotation_income3 +
+    crop_rotation_water_use +
+    crop_rotation_nutrition
   crop_rotation3_total_benefit <- vv(crop_rotation3_total_benefits_precal, n_year, var_CV=CV_value)
   
-
+  
   # Estimate the cost
   ##if farmers grow rice (assuming they grow rice 3 times per year)
-  rice_costs_precal <- sum(rice_farming_input_costs, rice_labor_costs,rice_machinery_costs)*3 +
-                            warehouse_establishment_costs + warehouse_maintenance_costs
+  rice_costs_precal <- (rice_farming_input_costs + rice_labor_costs + rice_machinery_costs)*3 +
+    warehouse_establishment_costs + warehouse_maintenance_costs
   rice_total_costs <- vv(rice_costs_precal, n_year,var_CV=CV_value)
   
   
   ##if farmers grow vegetables 
   ##(assuming they grow 3 different vegetables per year)
-  potato_costs_precal <- sum(potato_farming_input_costs, potato_labor_costs, potato_machinery_costs)
+  potato_costs_precal <- potato_farming_input_costs + potato_labor_costs + potato_machinery_costs
   potato_costs <- vv(potato_costs_precal, n_year, var_CV=CV_value)
   
-  shallot_costs_precal <- sum(shallot_farming_input_costs, shallot_labor_costs, shallot_machinery_costs)
+  shallot_costs_precal <- shallot_farming_input_costs + shallot_labor_costs + shallot_machinery_costs
   shallot_costs <- vv(shallot_costs_precal, n_year, var_CV=CV_value)
   
-  cabbage_costs_precal <- sum(cabbage_farming_input_costs, cabbage_labor_costs, cabbage_machinery_costs)
+  cabbage_costs_precal <- cabbage_farming_input_costs + cabbage_labor_costs + cabbage_machinery_costs
   cabbage_costs <- vv(cabbage_costs_precal, n_year, var_CV=CV_value)
   
-  vegetables_costs_precal <- sum(potato_costs, shallot_costs, cabbage_costs, 
-                                 vegetables_farming_equipment_costs,
-                                 warehouse_establishment_costs, 
-                                 warehouse_maintenance_costs)
+  vegetables_costs_precal <- potato_costs + shallot_costs + cabbage_costs + 
+    vegetables_farming_equipment_costs +
+    warehouse_establishment_costs +
+    warehouse_maintenance_costs
   vegetables_total_costs <- vv(vegetables_costs_precal, n_year, var_CV=CV_value)
   
   
   ##if farmers do crop rotation 
   ### total cost crop rotation 1 (potato-rice-shallot) 
-  crop_rotation_cost1_precal <- sum(potato_costs, rice_total_costs, shallot_costs)
+  crop_rotation_cost1_precal <- potato_costs + rice_total_costs + shallot_costs
   crop_rotation_cost1 <- vv(crop_rotation_cost1_precal, n_year, var_CV=CV_value)
-  crop_rotation1_total_costs_precal <- sum(crop_rotation_cost1, 
-                                            vegetables_farming_equipment_costs,
-                                            warehouse_establishment_costs, 
-                                            warehouse_maintenance_costs)
+  crop_rotation1_total_costs_precal <- crop_rotation_cost1 +
+    vegetables_farming_equipment_costs +
+    warehouse_establishment_costs +
+    warehouse_maintenance_costs
   crop_rotation1_total_costs <- vv(crop_rotation1_total_costs_precal, n_year, var_CV=CV_value)
   
   
   ### total benefits crop rotation 2 (potato-rice-cabbage)
-  crop_rotation_cost2_precal <- sum(potato_costs, rice_total_costs, cabbage_costs)
+  crop_rotation_cost2_precal <- potato_costs + rice_total_costs + cabbage_costs
   crop_rotation_cost2 <- vv(crop_rotation_cost2_precal, n_year, var_CV=CV_value)
-  crop_rotation2_total_costs_precal <- sum(crop_rotation_cost2, 
-                                           vegetables_farming_equipment_costs,
-                                           warehouse_establishment_costs, 
-                                           warehouse_maintenance_costs)
+  crop_rotation2_total_costs_precal <- crop_rotation_cost2 + 
+    vegetables_farming_equipment_costs +
+    warehouse_establishment_costs + 
+    warehouse_maintenance_costs
   crop_rotation2_total_costs <- vv(crop_rotation2_total_costs_precal, n_year, var_CV=CV_value)
   
   
   ### total benefits crop rotation 3 (shallot-rice-cabbage)
-  crop_rotation_cost3_precal <- sum(shallot_costs, rice_total_costs, cabbage_costs)
+  crop_rotation_cost3_precal <- shallot_costs + rice_total_costs + cabbage_costs
   crop_rotation_cost3 <- vv(crop_rotation_cost3_precal, n_year, var_CV=CV_value)
-  crop_rotation3_total_costs_precal <- sum(crop_rotation_cost3, 
-                                           vegetables_farming_equipment_costs,
-                                           warehouse_establishment_costs, 
-                                           warehouse_maintenance_costs)
+  crop_rotation3_total_costs_precal <- crop_rotation_cost3 +
+    vegetables_farming_equipment_costs +
+    warehouse_establishment_costs +
+    warehouse_maintenance_costs
   crop_rotation3_total_costs <- vv(crop_rotation3_total_costs_precal, n_year, var_CV=CV_value)
   
   
@@ -147,7 +147,7 @@ transition_rice_to_vegetables <- function(x, varnames){
   crop_rotation1_result <- crop_rotation1_total_benefit - crop_rotation1_total_costs
   crop_rotation2_result <- crop_rotation2_total_benefit - crop_rotation2_total_costs
   crop_rotation3_result <- crop_rotation3_total_benefit - crop_rotation3_total_costs
- 
+  
   
   # Calculate NPV
   
@@ -156,13 +156,13 @@ transition_rice_to_vegetables <- function(x, varnames){
                             calculate_NPV = TRUE)
   
   NPV_vegetables_interv <- discount(x = vegetables_result, 
-                            discount_rate = discount_rate,
-                            calculate_NPV = TRUE)
+                                    discount_rate = discount_rate,
+                                    calculate_NPV = TRUE)
   
   NPV_crop_rotation1_interv <- discount(x = crop_rotation1_result, 
                                         discount_rate = discount_rate,
                                         calculate_NPV = TRUE)
-   
+  
   NPV_crop_rotation2_interv <- discount(x = crop_rotation2_result, 
                                         discount_rate = discount_rate,
                                         calculate_NPV = TRUE)
@@ -185,10 +185,10 @@ transition_rice_to_vegetables <- function(x, varnames){
               cashflow_crop_rotation1 = crop_rotation1_result - rice_result,
               cashflow_crop_rotation2 = crop_rotation2_result - rice_result,
               cashflow_crop_rotation3 = crop_rotation3_result - rice_result))
-
+  
   
 }
-  
+
 
 
 # Run the Monte Carlo simulation using the model function
